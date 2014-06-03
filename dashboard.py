e5c7eaecdab13a3856650670b7468b2d39bd1605
Miguel Julián <mjulian@zentyal.com>
2014-06-04 01:13:40 +0200

    Added public tracker to main dashboard class

--- dashboard.py
+++ dashboard.py
@@ -6,31 +6,36 @@ from flask import Flask, render_template
 
 from configuration import ConfigurationParser
 from jenkins_parser import JenkinsHelper
+from redmine_parser import RedmineHelper
 
 class ModulesInfo:
-    def __init__(self):
-        if not self.cached_data_is_valid():
-            self.last_data_loaded = datetime.now()
-            self.load_jenkins_info()
-
     @classmethod
-    def cached_data_is_valid(self):
-        self.configuration = ConfigurationParser('dashboard.conf')
-        self.REFRESH_RATE = self.configuration.refresh_rate()
-        try:
-            time_difference = (datetime.now() - self.last_data_loaded).seconds
-        except AttributeError:
-            time_difference = self.REFRESH_RATE + 1
-            self.last_data_loaded = datetime.now()
+    def cached_data_is_valid(self, refresh_rate, module):
+        timestamp = 'last_' + module + '_loaded'
+        if hasattr(self, timestamp):
+            time_difference = (datetime.now() - getattr(self, timestamp)).seconds
+        else:
+            time_difference = refresh_rate + 1
 
-        return time_difference < self.REFRESH_RATE
+        return time_difference < refresh_rate
 
     @classmethod
     def load_jenkins_info(self):
-        self.configuration = ConfigurationParser('dashboard.conf')
-        zentyal_jenkins = JenkinsHelper(self.configuration)
-        self.jobs = zentyal_jenkins.get_jobs()
-        self.components = zentyal_jenkins.get_components()
+        configuration = ConfigurationParser('dashboard.conf')
+        if not self.cached_data_is_valid(configuration.refresh_rate(), 'jenkins'):
+            self.last_jenkins_loaded = datetime.now()
+            zentyal_jenkins = JenkinsHelper(configuration)
+            self.jobs = zentyal_jenkins.get_jobs()
+            self.components = zentyal_jenkins.get_components()
+
+
+    @classmethod
+    def load_public_tracker_info(self):
+        configuration = ConfigurationParser('dashboard.conf')
+        if not self.cached_data_is_valid(configuration.refresh_rate(), 'public_redmine'):
+            self.last_public_redmine_loaded = datetime.now()
+            url, key = configuration.public_tracker_credentials()
+            public_tracker = RedmineHelper(url, key)
 
 
 app = Flask(__name__)
@@ -38,6 +43,7 @@ app = Flask(__name__)
 @app.route("/")
 def dashboard():
     modules_info = ModulesInfo()
+    modules_info.load_jenkins_info()
 
     return render_template('dashboard.html',
                                 jobs = modules_info.jobs,
