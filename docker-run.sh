a9a2fa5260399c2330b52809ba71ae510756c4a3
Julio García <jjgarcia@zentyal.com>
2014-06-04 23:58:02 +0200

    Removing the need to download the code inside the container

--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker run -d --name="dashboard" -p 5000:5000 mjulian/dashboard /home/dashboard/dashboard-master/run.sh
+PATH_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+docker run -d -v $PATH_FOLDER:/home/dashboard:rw --name="dashboard" -p 5000:5000 mjulian/dashboard /home/dashboard/run.sh
