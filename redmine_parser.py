234dafecde37766ddfe56072845e8fcf70897dc6
Miguel Julián <mjulian@zentyal.com>
2014-06-05 08:56:48 +0200

    A matrix version/component of issues can be obtained

--- redmine_parser.py
+++ redmine_parser.py
@@ -13,11 +13,61 @@ class RedmineHelper:
 
     def initialize_attributes(self):
         self.issues = self.tracker.issue.all()
-        self._versions = {}
+        # Caching versions will speed up the loop (from 98" to 12" with 252 issues)
+        self._issues_versions()
 
+    def version_component_matrix(self):
+        matrix = {}
+        for issue in self.issues:
+            details = (str(issue.id), issue.url, issue.subject)
+            version = self._issue_version(issue)
+            component = self._issue_component(issue)
+
+            if version not in matrix:
+                matrix[version] = {}
+            if component not in matrix[version]:
+                matrix[version][component] = []
+
+            matrix[version][component].append(details)
+
+        return matrix
 
     def number_of_opened_issues(self):
         return len(self.issues)
+
+
+
+
+    # Private methods
+    def _issues_versions(self):
+        self._versions = {}
+        for issue in self.issues:
+            version_id = self._issue_version_id(issue)
+            if version_id not in self._versions:
+                self._versions[version_id] = self.tracker.version.get(version_id).name
+
+
+
+
+    # Getting single issue attributes (avoiding a "Issue" class)
+    def _issue_version_id(self, issue):
+        return issue.custom_fields.get(self.CUSTOM_FIELD_ID['version']).value
+
+    def _issue_version(self, issue):
+        version_id = issue.custom_fields.get(self.CUSTOM_FIELD_ID['version']).value
+        if version_id not in self._versions:
+            version = 'Unknown'
+        else:
+            version = self._versions[version_id]
+
+        return version
+
+    def _issue_component(self, issue):
+        component = issue.custom_fields.get(self.CUSTOM_FIELD_ID['component']).value
+        if not component:
+            component = "none"
+
+        return component
 
 # FIXME: only for development
 from configuration import ConfigurationParser
@@ -27,4 +77,5 @@ url, key = configuration.public_tracker_credentials()
 
 print "Gathering ticket information"
 public_tracker = RedmineHelper(url, key)
+print "Version/component matrix:\n" + str(public_tracker.version_component_matrix())
 print "Number of opened issues: " + str(public_tracker.number_of_opened_issues())
